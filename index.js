88124f3a8b6fb04fff8e90859bc10b6b72404f2c
arsrsyh <gotrany97@gmail.com>
2022-11-22 14:00:03 +0800

    backend

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const app = express()
 app.get('/', (req, res) => {
     return res.status(200).json({
         success: true,
-        message: "backend is running well"
+        message: "backend berjalan"
     })
 });
 
